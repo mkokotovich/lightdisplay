60698216346bbd1fb359972f7b4c082319704c44
Matthew Kokotovich <mkokotovich@gmail.com>
2016-10-28 15:44:37 -0500

    Added CommandHistory to arduino app

--- arduino/models.py
+++ arduino/models.py
@@ -3,3 +3,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class CommandHistory(models.Model):
+    command = models.CharField(max_length=200)
+    command_date = models.DateTimeField('date of command')
+
+    SUCCESS = 'SCS'
+    COULD_NOT_CONNECT_TO_ARDUINO = 'CNC'
+    COMMAND_STATUS_CHOICES = (
+        (SUCCESS, 'Success'),
+        (COULD_NOT_CONNECT_TO_ARDUINO, 'Could not connect to Arduino'),
+    )
+    command_status = models.CharField(max_length=3, choices=COMMAND_STATUS_CHOICES, default=SUCCESS)
+
+    def __str__(self):
+        return "{}: {} ({})".format(str(command_date), command, command_status)
--- lightdisplay/settings.py
+++ lightdisplay/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'arduino',
-    'videofeed',
-    'display',
+    'arduino.apps.ArduinoConfig',
+    'videofeed.apps.VideofeedConfig',
+    'display.apps.DisplayConfig',
 ]
 
 MIDDLEWARE = [
