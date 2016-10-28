5c2fc2f808e5c74816c7665082a9b561e98231ee
Matthew Kokotovich <mkokotovich@gmail.com>
2016-10-28 14:54:59 -0500

    Added display, videofeed apps

--- lightdisplay/urls.py
+++ lightdisplay/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^arduino/', include('arduino.urls')),
+    url(r'^display/', include('display.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('display.urls')),
 ]
