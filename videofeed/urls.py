c28e617f8377bfb80122a206b108d0f09756b427
Matthew Kokotovich <mkokotovich@gmail.com>
2016-11-07 16:27:31 -0600

    Added image frame to display a single image

--- videofeed/urls.py
+++ videofeed/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
+    url(r'^imageframe/', TemplateView.as_view(template_name="videofeed/imageframe.html"), name='imageframe'),
     url(r'^$', views.index, name='index'),
 ]
